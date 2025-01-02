476a08d59da71d24e86ab2a346f5d5455f6a1302
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-01-02 21:50:02 +0530

    stack

---
+++ stack.java
@@ -0,0 +1,64 @@
+public class stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push " + value);
+        } else {
+            stackArray[++top] = value;
+        }
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop");
+            return -1;
+        } else {
+            return stackArray[top--];
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek");
+            return -1;
+        } else {
+            return stackArray[top];
+        }
+    }
+
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+
+    public static void main(String[] args) {
+        stack stack = new stack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60); // This will show "Stack is full" message
+
+        System.out.println("Top element is: " + stack.peek());
+
+        while (!stack.isEmpty()) {
+            int value = stack.pop();
+            System.out.println("Popped: " + value);
+        }
+
+        stack.pop(); // This will show "Stack is empty" message
+    }
+}
