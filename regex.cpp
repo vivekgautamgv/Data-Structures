2fcd90d7ece718f954c7e72d42b74da75a3975a1
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-01-08 22:48:23 +0530

    dd

---
+++ regex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    regex pattern("^x.*yy$");
+    if (regex_match(str, pattern))
+        cout << "Valid" << endl;
+    else
+        cout << "Invalid" << endl;
+    return 0;
+}
--- single.java
+++ single.java
@@ -10,6 +10,7 @@ class Node{
 
 
 
+
 public class single {
     
 }
