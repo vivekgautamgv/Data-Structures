3a20ac36b88cc6939970c06f4390f9ff96eeffc0
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-01-18 23:26:17 +0530

    queue_2

--- queue.java
+++ queue.java
@@ -8,14 +8,14 @@ public class queue {
         queue.enqueue(50);
         queue.enqueue(60); // This will show "Queue is full" message
 
-        System.out.println("Front element is: " + queue.peek());
+        System.out.println("Front element is:" + queue.peek());
 
         while (!queue.isEmpty()) {
             int value = queue.dequeue();
             System.out.println("Dequeued: " + value);
         }
 
-        queue.dequeue(); // This will show "Queue is empty" message
+        queue.dequeue();
     }
 }
 
