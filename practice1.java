908621cdc20e8af0ed09bd5f9aee5af0a071f580
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-01-31 03:13:39 +0530

    practice

--- practice1.java
+++ practice1.java
@@ -4,6 +4,11 @@ public class practice1 {
     public static void main(String[] args) {
 
         System.out.println("hi");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(n);
     }
 
 }
