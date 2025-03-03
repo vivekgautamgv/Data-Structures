2a24f3c1ea165ac823f686df09c1d7e4f6de3a03
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-03-03 23:50:29 +0530

    trees

--- tree/tree.java
+++ tree/tree.java
@@ -4,23 +4,19 @@ import java.util.*;
 public class tree {
     
     static class Node{
-        int data;
-        Node left,right;
-        Node(int item){
-            data = item;
-            left = right = null;
-
-        }
-    }
-
-    class BinaryTree{
-        Node root;
+        
         
     }
 }
-Stack<Integer> s = new Stack<Integer>();
-Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
 
-int[] arr = new int[n];
+class TreeNode {
+    int val;
+    TreeNode left,right;
 
+    TreeNode(int x){
+        val =x;
+        left = right = null;
+    }
+}
+
+public class PreorderTraversal(TreeNode root)
