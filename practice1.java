382edc0e95fa6a4a5463c74d708ee0edf7f3615d
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-03-03 17:16:26 +0530

    work_2

--- practice1.java
+++ practice1.java
@@ -7,11 +7,34 @@ public class practice1 {
         int age = 30;
         int age2 = 25;
    
-        System.out.println("Hello World");
-        System.out.println("My name is " + name + " and I am " + age + " years old.");
-        System.out.println("My name is " + name2 + " and I am " + age2 + " years old.");
+  
         
-        
+        int[] arr = {1,2,3,4,6,3,5,24,25632534,23452,424,24,25,5};
+        Arrays.sort(arr);
+        System.out.print(Arrays.toString(arr));
+
+        int[] arr2 = new int[10];
+        arr2[0] = 97;
+        arr2[1] = 98;
+
+        Scanner sc = new Scanner(System.in);
+
+        int n  = new Scanner(System.in).nextInt();
+        System.out.println(n);
+
+        int[] arr3 = new int[n];
+
+        for (int i = 0;i<n;i++){
+            arr3[i] = sc.nextInt();
+    
+        }
+    
+        for (int i = 0;i<n;i++){
+            System.out.print(arr3[i]);
+    
+        }
+    
+    
     }
 
 }
