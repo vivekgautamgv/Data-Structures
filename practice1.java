c92b115e110b948870be198a7a75d4a33c93781b
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-03-03 23:03:28 +0530

    stack

--- practice1.java
+++ practice1.java
@@ -2,39 +2,13 @@ import java.util.*;
 
 public class practice1 {
     public static void main(String[] args) {
-        String name  = "Vivek";
-        String name2 = "Gagam";
-        int age = 30;
-        int age2 = 25;
-   
-  
-        
-        int[] arr = {1,2,3,4,6,3,5,24,25632534,23452,424,24,25,5};
-        Arrays.sort(arr);
-        System.out.print(Arrays.toString(arr));
+        Stack<Integer> s = new Stack<Integer>();
 
-        int[] arr2 = new int[10];
-        arr2[0] = 97;
-        arr2[1] = 98;
+        s.push(6);
+        s.push(570);
+        s.push(890);
+        s.push(590);
 
-        Scanner sc = new Scanner(System.in);
-
-        int n  = new Scanner(System.in).nextInt();
-        System.out.println(n);
-
-        int[] arr3 = new int[n];
-
-        for (int i = 0;i<n;i++){
-            arr3[i] = sc.nextInt();
-    
-        }
-    
-        for (int i = 0;i<n;i++){
-            System.out.print(arr3[i]);
-    
-        }
-    
-    
+        System.out.println(s);
     }
-
 }
--- tree/tree.java
+++ tree/tree.java
@@ -4,6 +4,23 @@ import java.util.*;
 public class tree {
     
     static class Node{
+        int data;
+        Node left,right;
+        Node(int item){
+            data = item;
+            left = right = null;
+
+        }
+    }
+
+    class BinaryTree{
+        Node root;
         
     }
 }
+Stack<Integer> s = new Stack<Integer>();
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+int[] arr = new int[n];
+
