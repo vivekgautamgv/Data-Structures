01ee380f9be7ab70abeba922df9af161f0594463
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-03-09 15:54:57 +0530

    sts

--- Javasts.java
+++ Javasts.java
@@ -416,8 +416,7 @@ import java.util.*;
 public class Javasts {
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, 5, 5, 6, 7, 8};
-        int tar = 15; // Example target sum
-
+        int tar = 15; 
         if (findTriplet(arr, tar)) {
             System.out.println("Triplet found.");
         } else {
@@ -429,13 +428,11 @@ public class Javasts {
         int n = arr.length;
 
         for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == tar) {
+           
+                    if (arr[i] + arr[i+2] + arr[i+3] == tar) {
                         return true;
                     }
-                }
-            }
+             
         }
         return false;
     }
