159212512692fb40ece1f08d538ddff6d3e543ea
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-03-19 08:25:04 +0530

    h

--- Javasts.java
+++ Javasts.java
@@ -457,3 +457,19 @@ import java.util.Scanner;
 // }
 
 
+import java.util.*;
+
+class Javasts{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
