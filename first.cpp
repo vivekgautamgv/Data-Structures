587c703bb9b782fb751c30a9b1d0cd5a55683021
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-04-05 11:26:43 +0530

    commit latest

--- first.cpp
+++ first.cpp
@@ -1,17 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void f(int i, int n){ 
-    if(i>n){
-        return; 
-    }
-
-    cout<<"vivek\n";
-    f(i+1,n);
-}
 int main(){
 
-   int n = 5;
-   f(1,n);
-   return 0;
+   cout<<"Hello World!"<<endl;
 }
