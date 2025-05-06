317532b783c262170d8cfe5e5c8f71a29bd66198
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-05-07 01:28:39 +0530

    nm

--- pra.java
+++ pra.java
@@ -4,3 +4,17 @@ public class pra {
         System.out.println(x++ + ++x);
     }
 }
+
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkediList { 
+    Node head;
+    
+}
