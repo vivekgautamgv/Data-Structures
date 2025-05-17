8c03f213aef8810d4e4795f8add41c99bd5d24ee
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-05-18 01:29:16 +0530

    day1

--- pra.java
+++ pra.java
@@ -1,20 +1,30 @@
-public class pra {
-    public static void main(String[] args){
-        int x = 5;
-        System.out.println(x++ + ++x);
-    }
-}
 
-class Node{
-    int data;
-    Node next;
-    public Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
 
-class LinkediList { 
-    Node head;
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in());
     
+    int n = sc.nextInt();
+
+    int n[] = new int[10];
+
+    for(int i = 0; i < n; i++){
+        n[i] = sc.nextInt();
+    }
+    for(int i = 0; i < n; i++){
+        System.out.print(n[i] + " ");
+    }
+    System.out.println();
+    for(int i = 0; i < n; i++){
+        System.out.print(n[i] + " ");
+    }
+    System.out.println();
+
+    Stack<Integer> stack = new Stack<>();
+    for(int i=0;i<n;i++)){
+        stack.push[n[i]];
+    }
+    
+    int z[] = {1, 2, 3, 4, 5};
+    Arrays.sort(z);
+    System.out.println(Arrays.toString(z));
 }
