1087b48a2227d308f321f25b84748d59e3049f74
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-05-26 01:27:59 +0530

    d

--- DSAC/s.cpp
+++ DSAC/s.cpp
@@ -1,47 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sum(int a , int b){
-    return a-b;
-}
-
-void print(){
-    cout<<"raj"<<endl;
-}
-
-
-void exppair(){
-    pair<int,int> p = {1,2};
-    cout<<p.first<<" "<<p.second<<endl;
-
-    pair<int, pair<int,int>> p1 = {1,{1,2}};
-    cout<<p1.first<<" "<<p1.second.first<<" "<<p1.second.second<<endl;
-
-    pair<int,int> p2[] = {{1,2},{2,3},{3,4}};
-    cout<<p2[0].first<<" "<<p2[0].second<<endl;
-
-
-
-}
-
-
 void vectorpair()
 {
-    vector<int> v;
+    
 
-    v.push_back(2);
-    v.emplace_back(3);
-    cout<<v[0]<<" "<<v[1]<<endl;
+    vector<int> j(2,100);
+    j.insert(j.begin(),300);
 
+    j.insert(j.begin()+1,4,200);
+    // for(int x :j){
+    //     cout<<x<<" ";
+    // }
+    for(auto x : j){
+        cout<<x<<" ";
+    }   
 
+    cout<<"\n"<<j.size()<<endl;
 
 }
+
+void deq(){
+
+    deque<int> d;
+    d.push_back(1);
+    d.push_front(2);
+    d.push_back(3); 
+    d.push_front(4);
+
+    for(auto x : d){
+        cout<<x<<" ";
+    }
+}
+
+void st(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+
+      while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+}
 int main(){
-    
-    print();
-    cout<<sum(10,20)<<endl;
+    st();
+    cout<<"\n"<<endl;
+    deq();
+    cout<<"\n"<<endl;
     vectorpair();
-    exppair();
     return 0;
 }
 
