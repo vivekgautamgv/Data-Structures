63fd3c719db68d79c14a686200334fba67c9c342
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-06-03 23:50:02 +0530

    pd

--- DSAC/tree.cpp
+++ DSAC/tree.cpp
@@ -1,1 +1,33 @@
 #include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr),right(nullptr) {}
+};
+
+void preorder(Node* root){
+    if(root == nullptr) return ;
+    cout << root->data << " "; 
+    preorder(root->left);
+     
+    preorder(root->right);
+}
+
+int main(){
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(0);
+    cout << "Binary tree created successfully." << endl;
+    preorder(root);
+    cout << endl;
+
+
+
+}
