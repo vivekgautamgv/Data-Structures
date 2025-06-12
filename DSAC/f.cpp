d1247d3d1cb676868b4e66ef647e5d5f65a5e509
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-06-13 01:34:52 +0530

    chanegs

--- DSAC/f.cpp
+++ DSAC/f.cpp
@@ -1,28 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int di(int dive, int divi) {
+    int count = 0;
 
-int di(int dive,int divi){
-
-        
-    while(dive-divi> divi){
-        int left = dive-divi;
-        int count++;
-        if(left == 0){
-            return count;
-        }
-
-        else if (left<divi){
-            return count;
-        }
-    
+    while (dive >= divi) {
+        dive = dive - divi;
+        count++;
     }
 
-    }
-int main(){
+    return count;
+}
+
+int main() {
     int n = 40;
-    int m=8;
-    int result = di(n,m);
+    int m = 8;
+    int result = di(n, m);
     cout << "The result of dividing " << n << " by " << m << " is: " << result << endl;
-    
+
+    return 0;
 }
